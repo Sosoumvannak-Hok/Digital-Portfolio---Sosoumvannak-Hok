ba49d3c9906ca28210a73b1da21f3017ab3104fe
Hok Sosoumvannak <555068@cart.ads>
2018-03-19 15:05:27 -0700

    3/19

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 /* ------------------------------------- SLIDESHOW --------------------------------------- */
+
+var currentSlide = 1;
+var totalSlide = document.getElementsByClassName("slide");
+
+function prev() {
+    if (currentSlide > 1) {
+        currentSlide--;
+    } else {
+        currentSlide = totalSlide.length;
+    }
+    console.log(currentSlide);
+    changeSlide(currentSlide);
+}
+
+function next() {
+    if (currentSlide < totalSlide.length) {
+        currentSlide++;
+    } else {
+        currentSlide = 1;
+    }
+    console.log(currentSlide);
+    changeSlide(currentSlide);
+}
+
+function changeSlide(num) {
+    for (i = 0; i < totalSlide.length; i++) {
+        totalSlide[i].style.display = "none";
+    }
+    totalSlide[num - 1].style.display = "block";
+}
