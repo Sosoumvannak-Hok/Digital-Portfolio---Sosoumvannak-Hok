7489c05325c4dc68e3c13db3cd7f83f52b740b5b
unknown <sosoumvannakhok@gmail.com>
2018-03-22 20:56:08 -0700

    3/22 home

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+/* ------------------------------------- SLIDEBAR ---------------------------------------- */
+
+function toggleSidebar() {
+    
+    sidebar = document.getElementById("sidebar");
+    
+    if (sidebar.style.display === "block") {
+        sidebar.style.display = "none";
+    } else {
+        sidebar.style.display = "block";
+    }
+}
+
 /* ------------------------------------- SLIDESHOW --------------------------------------- */
 
 var currentSlide = 1;
@@ -9,6 +22,7 @@ function prev() {
     } else {
         currentSlide = totalSlide.length;
     }
+    
     changeSlide(currentSlide);
 }
 
@@ -18,6 +32,7 @@ function next() {
     } else {
         currentSlide = 1;
     }
+    
     changeSlide(currentSlide);
 }
 
@@ -25,19 +40,45 @@ function changeSlide(num) {
     for (i = 0; i < totalSlide.length; i++) {
         totalSlide[i].style.display = "none";
     }
+    
     totalSlide[num - 1].style.display = "block";
 }
 
-/* ------------------------------------- DISPLAY ----------------------------------------- */
+/* --------------------------------- DISPLAY & READ -------------------------------------- */
 
 var totalDisplay = document.getElementsByClassName("display");
+var totalRead = document.getElementsByClassName("read");
+
+function disappearTotalRead() {
+    for (i = 0; i < totalRead.length; i++) {
+        totalRead[i].style.display = "none";
+    }
+}
 
 function appearDisplay(type) {
-    
     for (i = 0; i < totalDisplay.length; i++) {
         totalDisplay[i].style.display = "none";
     }
     
-    console.log(type);
     document.getElementsByClassName(type)[0].style.display = "flex";
+    
+    if (type == 'display_lor') {
+        document.getElementById("read_container").style.display = "block";
+    } else {
+        disappearTotalRead();
+        
+        if (document.getElementById("read_container") !== null) {
+            document.getElementById("read_container").style.display = "none";
+        }
+    }
+}
+
+function appearRead(type) {
+    disappearTotalRead();
+    document.getElementsByClassName(type)[0].style.display = "flex";
+}
+
+function closeButton(type) {
+    disappearTotalRead();
+    document.getElementsByClassName(type)[0].style.display = "none";
 }
